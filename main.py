32c23d0c44456977e86aadcaf30149fee0bb6c34
Nehal Jain <nehalj273@gmail.com>
2026-06-07 22:35:32 +0530

    Improve password generator with validation and secure character mix

--- main.py
+++ main.py
@@ -2,19 +2,29 @@
 import random
 import string
 
+print("Welcome to the Password Generator!")
+print("Generate secure random passwords instantly.\n")
 
 special_chars = "!@#$%&*"
 characters = string.ascii_letters + string.digits + special_chars
 
+password = [random.choice(string.ascii_uppercase),
+random.choice(string.ascii_lowercase),
+random.choice(string.digits),
+random.choice(special_chars)]
+
 length = int(input("Enter the length of password you need to generate:"))
 
-if length <= 0:
-    print("Password length must be greater than 0")
+if length < 4:
+    print("Password length must be atleast 4")
 else :
-  password = ""
+  
 
-  for i in range(length):
-    password += random.choice(characters)
+  for i in range(length-4):
+    password.append(random.choice(characters))
+
+  random.shuffle(password)
+  password = "".join(password)
 
 
   print(f"Generated password :  {password}")
